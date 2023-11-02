cbe5593617ad882f03c134827720142b644b442d
Fernando Barboza <falandocomofernando@gmail.com>
2023-11-02 17:25:08 -0300

    add a target that spawns randomly across the window

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 ///////////////////////////////////////////////////////////////////////////////
 // Global variables
@@ -14,7 +15,7 @@ SDL_Window *window = NULL;
 SDL_Renderer *renderer = NULL;
 
 ///////////////////////////////////////////////////////////////////////////////
-// Declare two game objects for the ball and the paddle
+// Declare two game objects for the ball and the target
 ///////////////////////////////////////////////////////////////////////////////
 struct GameObject {
   f32 x;
@@ -23,7 +24,7 @@ struct GameObject {
   f32 height;
   f32 vel_x;
   f32 vel_y;
-} ball, paddle;
+} ball, target;
 typedef struct GameObject GameObject;
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -35,7 +36,7 @@ int initialize_window(void) {
     return false;
   }
   window =
-      SDL_CreateWindow("Asteroids", SDL_WINDOWPOS_CENTERED,
+      SDL_CreateWindow("Square Eater", SDL_WINDOWPOS_CENTERED,
                        SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, 0);
   if (!window) {
     fprintf(stderr, "Error creating SDL Window.\n");
@@ -56,13 +57,23 @@ GameObject setup(void) {
   SDL_DisplayMode DM;
   SDL_GetCurrentDisplayMode(0, &DM);
 
-  // Initialize the ball object moving down at a constant velocity
-  ball.x = (WINDOW_WIDTH / 2.0);
-  ball.y = (WINDOW_HEIGHT / 2.0);
-  ball.width = 20;
-  ball.height = 20;
+  // Initialize the ball object
+  ball.x = (WINDOW_WIDTH >> 1);
+  ball.y = (WINDOW_HEIGHT >> 1);
+  ball.width = 15;
+  ball.height = 15;
   ball.vel_x = 80;
   ball.vel_y = 80;
+
+  srand(time(NULL));
+
+  // Initialize the target object
+  target.x = rand() % (WINDOW_WIDTH + 1);
+  target.y = rand() % (WINDOW_HEIGHT + 1);
+  target.width = 10;
+  target.height = 10;
+  target.vel_x = 0;
+  target.vel_y = 0;
 
   return ball;
 }
@@ -72,10 +83,10 @@ GameObject setup(void) {
 ///////////////////////////////////////////////////////////////////////////////
 void update(void) {
   // Get delta_time factor converted to seconds to be used to update objects
-  f32 delta_time = (SDL_GetTicks() - last_frame_time) / 1000.0;
+  // f32 delta_time = (SDL_GetTicks() - last_frame_time) / 1000.0;
 
   // Store the milliseconds of the current frame to be used in the next one
-  last_frame_time = SDL_GetTicks();
+  // last_frame_time = SDL_GetTicks();
 
   // Move ball as a function of delta time
   // ball.x += ball.vel_x * delta_time;
@@ -108,11 +119,16 @@ void render(void) {
   SDL_RenderClear(renderer);
 
   // Draw a rectangle for the ball object
-  SDL_Rect ball_rect = {(int)ball.x, (int)ball.y, (int)ball.width,
-                        (int)ball.height};
+  SDL_Rect ball_rect = {(u16)ball.x, (u16)ball.y, (u16)ball.width,
+                        (u16)ball.height};
+
+  SDL_Rect target_rect = {(u16)target.x, (u16)target.y, (u16)target.width,
+                          (u16)target.height};
 
   SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
   SDL_RenderFillRect(renderer, &ball_rect);
+  SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+  SDL_RenderFillRect(renderer, &target_rect);
 
   SDL_RenderPresent(renderer);
 }
@@ -129,7 +145,7 @@ void destroy_window(void) {
 ///////////////////////////////////////////////////////////////////////////////
 // Function to poll SDL events and process keyboard input
 ///////////////////////////////////////////////////////////////////////////////
-void process_input(GameObject *go) {
+void process_input(GameObject *game_obj) {
   SDL_Event event;
   while (SDL_PollEvent(&event)) {
     switch (event.type) {
@@ -155,16 +171,15 @@ void process_input(GameObject *go) {
       break;
     }
   }
-  render();
 }
 
 int main(int argc, char **args) {
   game_is_running = initialize_window();
 
-  GameObject go = setup();
+  GameObject game_obj = setup();
 
   while (game_is_running) {
-    process_input(&go);
+    process_input(&game_obj);
     update();
     render();
   }
