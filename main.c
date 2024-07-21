b06f230e9963c5b80eeb5d1502a353c35c5c589b
barbozafernando <fernando.barboza@gazin.com.br>
2024-07-21 14:48:10 -0300

    feat: add fps counter

--- main.c
+++ main.c
@@ -8,6 +8,7 @@
 #include <SDL2/SDL_ttf.h>
 #include <SDL2/SDL_video.h>
 #include <SDL2/SDL_image.h>
+#include <SDL2/SDL_timer.h>
 #include <SDL2/SDL_render.h>
 #include <SDL2/SDL_surface.h>
 
@@ -26,21 +27,58 @@ typedef struct {
   int           game_is_running;
   int           last_frame_time;
   uint8_t       score;
-  float         delta_time;
-  TTF_Font*     font36;
+  TTF_Font*     font;
   SDL_Window*   window;
   SDL_Renderer* renderer;
   Player        player;
   Player        target;
 } Game;
 
-void draw_scoreboard(Game* game) {
-  const char *FONT_PATH = "./assets/Roboto-Regular.ttf";
+float calculate_fps(uint32_t startTick, uint32_t endTick) {
+    float elapsedTime = (endTick - startTick) / 1000.0f;
+    return elapsedTime > 0 ? 1.0f / elapsedTime : 0;
+}
+
+void draw_fps(Game* game, float fps) {
+  const char FONT_PATH[] = "./assets/Roboto-Regular.ttf";
   SDL_Color text_color = {255, 255, 255, 255};
 
-  game->font36 = TTF_OpenFont(FONT_PATH, 36);
+  game->font = TTF_OpenFont(FONT_PATH, 16);
 
-  if (game->font36 == NULL) {
+  if (game->font == NULL) {
+    fprintf(stderr, "Font was not loaded: %s", TTF_GetError());
+  }
+
+  // Create a text buffer for the score
+  char score_text[255];
+
+  // Convert the score integer to a string
+  snprintf(score_text, sizeof(score_text), "FPS: %.2f", fps);
+
+  SDL_Surface *text_surface =
+      TTF_RenderText_Blended(game->font, score_text, text_color);
+  SDL_Texture *text_texture =
+      SDL_CreateTextureFromSurface(game->renderer, text_surface);
+
+  SDL_Rect fps_counter = {(WINDOW_WIDTH - 100), 0, FPS_COUNTER_WIDTH, FPS_COUNTER_HEIGHT};
+
+  SDL_RenderCopy(game->renderer, text_texture, NULL, &fps_counter);
+
+  SDL_RenderPresent(game->renderer);
+
+  SDL_FreeSurface(text_surface);
+  SDL_DestroyTexture(text_texture);
+
+  TTF_CloseFont(game->font);
+}
+
+void draw_scoreboard(Game* game) {
+  const char FONT_PATH[] = "./assets/Roboto-Regular.ttf";
+  SDL_Color text_color = {255, 255, 255, 255};
+
+  game->font = TTF_OpenFont(FONT_PATH, 36);
+
+  if (game->font == NULL) {
     fprintf(stderr, "Font was not loaded: %s", TTF_GetError());
   }
 
@@ -51,7 +89,7 @@ void draw_scoreboard(Game* game) {
   snprintf(score_text, sizeof(score_text), "SCORE: %d", game->score);
 
   SDL_Surface *text_surface =
-      TTF_RenderText_Solid(game->font36, score_text, text_color);
+      TTF_RenderText_Solid(game->font, score_text, text_color);
   SDL_Texture *text_texture =
       SDL_CreateTextureFromSurface(game->renderer, text_surface);
 
@@ -64,7 +102,7 @@ void draw_scoreboard(Game* game) {
   SDL_FreeSurface(text_surface);
   SDL_DestroyTexture(text_texture);
 
-  TTF_CloseFont(game->font36);
+  TTF_CloseFont(game->font);
 }
 
 int initialize_window(Game* game) {
@@ -133,17 +171,6 @@ void update(Game* game) {
     game->player.y = 0;
   }
 
-  // Get a delta time factor converted to seconds to be used to update my
-  // objects later
-  game->delta_time = (SDL_GetTicks() - game->last_frame_time) / 1000.0f;
-
-  game->last_frame_time = SDL_GetTicks();
-
-  // for (int i = 0; i < 4; i++) {
-  //   player.x += FPS * delta_time;
-  //   player.y += FPS * delta_time;
-  // }
-
   if ((game->player.x + game->player.width >= game->target.x) &&
       (game->target.x + game->target.width >= game->player.x) &&
       (game->player.y + game->player.height >= game->target.y) &&
@@ -169,6 +196,8 @@ void render(Game* game) {
   SDL_RenderFillRect(game->renderer, &player_rect);
   SDL_SetRenderDrawColor(game->renderer, 255, 0, 0, 255);
   SDL_RenderFillRect(game->renderer, &target_rect);
+
+  draw_scoreboard(game);
 }
 
 void process_input(Game* game) {
@@ -205,24 +234,32 @@ void process_input(Game* game) {
 }
 
 int main() {
-  static Game game_instance;
+  static Game   game_instance;
   static Player player;
   static Target target;
 
   game_instance.game_is_running = initialize_window(&game_instance);
   game_instance.last_frame_time = 0;
   game_instance.score           = 0;
-  game_instance.delta_time      = 0.0;
   game_instance.player          = player;
   game_instance.target          = target;
+
+  float fps;
+  uint32_t start_tick, end_tick;
 
   setup(&game_instance);
 
   while (game_instance.game_is_running) {
+    start_tick = SDL_GetTicks();
+
     process_input(&game_instance);
     update(&game_instance);
     render(&game_instance);
-    draw_scoreboard(&game_instance);
+
+    end_tick = SDL_GetTicks();
+    fps = calculate_fps(start_tick, end_tick);
+    draw_fps(&game_instance, fps);
+    SDL_Delay(16);
   }
 
   TTF_Quit();
